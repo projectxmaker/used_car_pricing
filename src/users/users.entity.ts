2c6d49a68076c7b51c296170ba7204f37c5697f7
Tuan <projectxmaker@gmail.com>
2022-06-09 13:54:15 +0700

    add hook AfterInsert, AfterUpdate, AfterRemove into User entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +10,19 @@ export class User {
 
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log("Inserted User w/ id " + this.id);
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log("Removed User w/ id " + this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log("Updated User w/ id " + this.id);
+    }
 }
