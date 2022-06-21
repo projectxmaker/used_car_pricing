4fccd13d61bbc80cb6dadf419c69fa1fe4ed5531
Tuan <projectxmaker@gmail.com>
2022-06-21 11:24:08 +0700

    prepare for heroku deployment

--- db.datasourceoptions.ts
+++ db.datasourceoptions.ts
@@ -20,6 +20,14 @@ switch (process.env.NODE_ENV) {
         });
         break;
     case 'production':
+        Object.assign(_DBOptions, {
+            type: 'posgres',
+            url: process.env.DATABASE_URL,
+            migrationsRun: true,
+            ssl: {
+                rejestUnauthorized: false
+            }
+        });
         break;
     default:
         throw new Error('unknown environment');
